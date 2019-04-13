038be5a838e24b4e062550c12a1efe4a6f5b09ae
shuangqi <hilishuangqi@hotmail.com>
2019-04-13 14:45:21 +0800

    fix config

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@
 ./gradlew build
 
 DOCKER_REGISTRY=076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn
-SERVICE_NAME=parking-service
+SERVICE_NAME=justice-league-ticket-service
 VERSION=1.0.$BUILD_NUMBER
 DOCKER_IMAGE=$DOCKER_REGISTRY/$SERVICE_NAME:$VERSION
 
 sudo $(aws ecr get-login --no-include-email --region cn-northwest-1)
 
-sudo docker build -t parking-service .
-sudo docker tag parking-service:latest $DOCKER_REGISTRY/$SERVICE_NAME:latest
-sudo docker tag parking-service:latest $DOCKER_IMAGE
+sudo docker build -t justice-league-ticket-service .
+sudo docker tag justice-league-ticket-service:latest $DOCKER_REGISTRY/$SERVICE_NAME:latest
+sudo docker tag justice-league-ticket-service:latest $DOCKER_IMAGE
 sudo docker push $DOCKER_REGISTRY/$SERVICE_NAME:latest
 sudo docker push $DOCKER_IMAGE
 
